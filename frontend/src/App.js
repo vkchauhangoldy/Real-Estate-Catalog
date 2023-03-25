e0721e614718717e9b0bc01101dac7028cb3dba8
VINAY KUMAR CHAUHAN <vkchauhan750@gmail.com>
2023-03-25 20:28:28 +0530

    file updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import Signin from './components/signin/Signin';
+import Signup from "./components/signup/Signup";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Signin />} />
+    <Route path='/signup' element={<Signup/>} />
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
