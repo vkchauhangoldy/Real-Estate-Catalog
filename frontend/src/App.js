084280b975b844896e0a517aaf4e810effd40ef6
Shyam Srinivasan M <kmshyam7991@gmail.com>
2023-03-29 08:31:42 +0530

    Frontend added successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainNavigation from "./components/Navigation/MainNavigation";
+import HomePage from "./components/HomePage/HomePage";
+import PropertyNavbar from "./components/PropertyNavbar/PropertyNavbar";
+import BasicInfoForm from "./components/PropertyForms/BasicInfoForm";
+import PropertyDetailForm from "./components/PropertyForms/PropertyDetailForm";
+import GeneralInfoForm from "./components/PropertyForms/GeneralInfoForm";
+import LocationInfoForm from "./components/PropertyForms/LocationInfoForm";
+import SignUpForm from "./components/UserAuthForms/SignUpForm";
+import SignInForm from "./components/UserAuthForms/SignInForm";
+import MainPage from "./components/MainPage/MainPage";
+import { checkAuthLoader } from "./utils/auth";
+import EditPropertyForm from "./components/PropertyForms/EditPropertyForm";
+import UserPropertyPage from "./components/HomePage/UserPropertyPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <MainPage /> },
+  {
+    path: "user",
+    element: <MainNavigation />,
+    loader: checkAuthLoader,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "property", element: <UserPropertyPage /> },
+      {
+        path: "property",
+        element: <PropertyNavbar />,
+        children: [
+          { path: "basic-info", element: <BasicInfoForm /> },
+          { path: "property-detail", element: <PropertyDetailForm /> },
+          { path: "general-info", element: <GeneralInfoForm /> },
+          { path: "location-info", element: <LocationInfoForm /> },
+          { path: "edit", element: <EditPropertyForm /> },
+        ],
+      },
+    ],
+  },
+  { path: "auth/signup", element: <SignUpForm /> },
+  { path: "auth/signin", element: <SignInForm /> },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/components/Navigation/SideNavbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const SideNavebarList = (props) => {
+  const navigateHandler = () => {
+    props.onClick(props.listName);
+  };
+
+  return (
+    <li className="sidenav-hover" onClick={navigateHandler}>
+      <FontAwesomeIcon icon={props.icon} style={{ color: `${props.color}` }} />
+      <p style={{ color: `${props.color}` }}>{props.listName}</p>
+    </li>
+  );
+};
+
+export default SideNavebarList;
