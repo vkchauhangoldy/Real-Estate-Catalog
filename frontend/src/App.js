4243fc60fc205dc72da02b5fa697fef80b4c0afb
VINAY KUMAR CHAUHAN <vkchauhan750@gmail.com>
2023-03-29 06:02:42 +0530

    deployed successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,47 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
-import Signin from './components/signin/Signin';
-import Signup from "./components/signup/Signup";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainNavigation from "./components/Navigation/MainNavigation";
+import HomePage from "./components/HomePage/HomePage";
+import PropertyNavbar from "./components/PropertyNavbar/PropertyNavbar";
+import BasicInfoForm from "./components/PropertyForms/BasicInfoForm";
+import PropertyDetailForm from "./components/PropertyForms/PropertyDetailForm";
+import GeneralInfoForm from "./components/PropertyForms/GeneralInfoForm";
+import LocationInfoForm from "./components/PropertyForms/LocationInfoForm";
+import SignUpForm from "./components/UserAuthForms/SignUpForm";
+import SignInForm from "./components/UserAuthForms/SignInForm";
+import MainPage from "./components/MainPage/MainPage";
+import { checkAuthLoader } from "./utils/auth";
+import EditPropertyForm from "./components/PropertyForms/EditPropertyForm";
+import UserPropertyPage from "./components/HomePage/UserPropertyPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <MainPage /> },
+  {
+    path: "user",
+    element: <MainNavigation />,
+    loader: checkAuthLoader,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "property", element: <UserPropertyPage /> },
+      {
+        path: "property",
+        element: <PropertyNavbar />,
+        children: [
+          { path: "basic-info", element: <BasicInfoForm /> },
+          { path: "property-detail", element: <PropertyDetailForm /> },
+          { path: "general-info", element: <GeneralInfoForm /> },
+          { path: "location-info", element: <LocationInfoForm /> },
+          { path: "edit", element: <EditPropertyForm /> },
+        ],
+      },
+    ],
+  },
+  { path: "auth/signup", element: <SignUpForm /> },
+  { path: "auth/signin", element: <SignInForm /> },
+]);
+
 function App() {
-  return (
-    <>
-    <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Signin />} />
-    <Route path='/signup' element={<Signup/>} />
-    </Routes>
-    </BrowserRouter>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
